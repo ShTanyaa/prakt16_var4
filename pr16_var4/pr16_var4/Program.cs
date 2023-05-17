0eb79d4081100522bd3f219518f2adbdfd61441f
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-05-17 16:39:22 +0500

    Add files via upload

---
+++ pr16_var4/pr16_var4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr16_var4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string s1 = Console.ReadLine();
+                string s2 = Console.ReadLine();
+
+            if (s1.Length != s2.Length)
+                Console.WriteLine("Строки не обратны друг другу");
+            bool flag = true;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] != s2[s2.Length - i - 1])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag)
+                Console.WriteLine("Строки являются обратными друг другу.");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ pract16_var4/pract16_var4/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace pract16_var4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* Queue q = new Queue();
+             Queue q1 = new Queue();
+             StreamReader sr = new StreamReader("t.txt");
+             foreach (char s in sr.ReadLine())
+             {
+                 int number = int.Parse(s);
+                 if (number >= 0) q.Enqueue(number);
+                 else q1.Enqueue(number);
+             }*/
+
+            int[] positiveNumbers = new int[100];
+            int[] negativeNumbers = new int[100];
+            int positiveCount = 0;
+            int negativeCount = 0;
+            StreamReader sr = new StreamReader("t.txt");
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                int num = int.Parse(line);
+                if (num >= 0)
+                {
+                    positiveNumbers[positiveCount++] = num;
+                    
+                }
+                else
+                    negativeNumbers[negativeCount++] = num;
+            }
+            for (int i = 0; i < positiveCount; i++)
+            {
+                listBox1.Items.Add(positiveCount);
+            }
+            for (int i = 0; i < negativeCount; i++)
+            {
+                listBox1.Items.Add(negativeNumbers);
+            }
+        }
+    }
+}
+
